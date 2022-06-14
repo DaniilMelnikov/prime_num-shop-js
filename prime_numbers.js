1a93b0b0d2f5617f739ac3cd3bb79babe4e4f132
Daniil Melnikov <dnl.melnikov@yandex.ru>
2022-06-14 22:03:47 +0700

    Ready code v1.1

--- prime_numbers.js
+++ prime_numbers.js
@@ -1,18 +1,19 @@
 function primeNum(n) {
+    if (n == 0) {
+        return primeNum.arr;
+    }
     if (!primeNum.arr && !primeNum.counter) {
         primeNum.arr = [];
         primeNum.counter = 1;
     }
     primeNum.counter++
-    for (i = 2; n != 0; i++) {
-        if (primeNum.counter % i == 0 && primeNum.counter != 2) {
+    for (i = 2; i < primeNum.counter; i++) {
+        if (primeNum.counter % i == 0) {
             return primeNum(n);
         }
-        primeNum.arr.push(primeNum.counter)
-        return primeNum(n-1);
     }
-    return primeNum.arr;
-    
+    primeNum.arr.push(primeNum.counter)
+    return primeNum(n-1);
 }
 
 console.log(primeNum(process.argv[2]));
